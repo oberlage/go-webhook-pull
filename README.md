d41b44aeff663df479ae1960f1f480a1c27ede0d
Oberon Berlage <git@oberonberlage.nl>
2018-09-21 17:45:25 +0200

    add pullpath function, added validation, added info endpoint

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # go-webhook-pull
 
-## Prerequisites
-This (very basic) tool requires that you have a git repository already set up and authentication to the git server is handled (i.e. via SSH).
-To test if this is the case you a simple `git pull` on the command line in the repository should work (the executable should run from within the main folder of the local git repo).
+go-webhook-pull is a very simple script that can be used to pull-on-push from a git repository.
+You can configure your webhook so that this listener is called, and it wil `git pull` from the repository.
 
 ## Compiling
-To compile this script use the `go build` command. You can build it for specific OS and architecture, please refer to [this blog post of Dave Cheney][1].
+To compile this script use the `./build.sh` script that is included in the repo. You can append your specific OS and architecture, please refer to [this blog post of Dave Cheney][1] for more info.
+
+## Setup
+You have to setup your git repository as usual (by using `git clone` and checking out the branch you want). This script assumes that authentication is handled, i.e. via SSH keys. 
+
+To test if your repository is ready for this script a simple `git pull` from the command line should work without any errors.
 
 ## Usage
-Start on commandline by `TOKEN=abc123 PORT=1337 ./go-webhook-pull &`.
-You can ommit the env variables (Defaults: no token auth, port 8080).
+Start on commandline by `TOKEN=abc123 PORT=1337 PULLPATH="public_html" ./go-webhook-pull &`.
+You can ommit the env variables (Defaults: no token auth, port 8080, pullpath path of executable).
+
+You can configure your webhooks to point to: `http://X.X.X.X:PORT/pull` (either GET or POST)
+To check the current version of this listener: `http://X.X.X.X:PORT/info` (GET)
 
 [1]: https://dave.cheney.net/2015/08/22/cross-compilation-with-go-1-5
--- main.go
+++ main.go
@@ -5,16 +5,32 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 )
 
 var router *gin.Engine
+var version = "1.0.0"
+
+var validPullPath string
+
+func init() {
+
+	var pullPathValid bool
+	pullPath := os.Getenv("PULLPATH")
+	pullPathValid, validPullPath = isValidPullPath(pullPath)
+	if !pullPathValid {
+		fmt.Printf("Error: pullpath `%s` is not valid or does not exist.\n", pullPath)
+		os.Exit(1)
+	}
+
+	doPull()
+}
 
 func main() {
-
 	gin.SetMode(gin.ReleaseMode)
-	router = gin.Default()
+	router = gin.New()
 
 	initializeRoutes()
 	router.Run() // listen and serve on 0.0.0.0:8080
@@ -22,6 +38,8 @@ func main() {
 
 func initializeRoutes() {
 	router.GET("/pull", handlePull)
+	router.POST("/pull", handlePull)
+	router.GET("/info", handleInfo)
 }
 
 // correctToken validates if a givenToken is correct
@@ -33,6 +51,45 @@ func correctToken(givenToken string) bool {
 		return false
 	}
 	return true
+}
+
+// directoryExists checks if the specified directory exists on the filesystem
+func directoryExists(filePath string) (exists bool) {
+	exists = true
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		exists = false
+	}
+
+	return
+}
+
+// isValidPullPath checks wether the specified pullpath is a valid string and if it exists
+// when input variable pullPath is empty, is it assumed to be valid and set to ./ in the output variable.
+func isValidPullPath(pullPath string) (isValid bool, validPath string) {
+	re := regexp.MustCompile(`(?m)^[a-zA-Z0-9_\- ]+$`)
+
+	if re.MatchString(pullPath) && directoryExists(pullPath) {
+		isValid = true
+		validPath = fmt.Sprintf("%s/", pullPath)
+		return
+	}
+
+	if pullPath == "" {
+		isValid = true
+		validPath = "./"
+		return
+	}
+
+	isValid = false
+	return
+}
+
+// doPull performs the git pull command
+func doPull() (errCR error) {
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd %s; git pull;", validPullPath))
+	errCR = cmd.Run()
+	return
 }
 
 // handlePull handles the pull command and returns HTPP 200 on success with json {"status":"Done"}
@@ -47,8 +104,7 @@ func handlePull(c *gin.Context) {
 		return
 	}
 
-	cmd := exec.Command("git", "pull")
-	errCR := cmd.Run()
+	errCR := doPull()
 
 	if errCR != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("%v", errCR)})
@@ -56,4 +112,8 @@ func handlePull(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"status": "Done"})
+}
+
+func handleInfo(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"version": version})
 }
